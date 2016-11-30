84719a97881d535371181eaa286dfbaa658a1322
Jonathan Bliss <blissjonathan1@gmail.com>
2016-11-30 13:20:00 -0500

    Client Update

--- src/Client/ClientWindow.java
+++ src/Client/ClientWindow.java
@@ -3,10 +3,22 @@ package Client;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JComboBox;
+import javax.swing.JList;
+import javax.swing.AbstractListModel;
+import javax.swing.JTextField;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class ClientWindow {
 
-	private JFrame frame;
+	private JFrame frmClient;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -16,7 +28,7 @@ public class ClientWindow {
 			public void run() {
 				try {
 					ClientWindow window = new ClientWindow();
-					window.frame.setVisible(true);
+					window.frmClient.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,9 +47,62 @@ public class ClientWindow {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmClient = new JFrame();
+		frmClient.setTitle("Client");
+		frmClient.setBounds(100, 100, 430, 407);
+		frmClient.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		frmClient.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Check In");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		btnNewButton.setBounds(10, 27, 89, 23);
+		panel.add(btnNewButton);
+		
+		JList list = new JList();
+		list.setModel(new AbstractListModel() {
+			String[] values = new String[] {"1", "2", "3", "4"};
+			public int getSize() {
+				return values.length;
+			}
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		list.setBounds(200, 20, 204, 64);
+		panel.add(list);
+		
+		textField = new JTextField();
+		textField.setBounds(200, 95, 204, 238);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JButton button = new JButton("Check Out");
+		button.setBounds(10, 61, 89, 23);
+		panel.add(button);
+		
+		JButton button_1 = new JButton("Refresh");
+		button_1.setBounds(10, 100, 89, 23);
+		panel.add(button_1);
+		
+		JButton button_2 = new JButton("Latest");
+		button_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		button_2.setBounds(10, 136, 89, 23);
+		panel.add(button_2);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frmClient.setJMenuBar(menuBar);
+		
+		JMenuItem menuItem = new JMenuItem("File");
+		menuBar.add(menuItem);
 	}
-
 }
