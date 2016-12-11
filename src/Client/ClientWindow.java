1b747672199b47dd73b88021293cc504fab0ae99
Jonathan Bliss <blissjonathan1@gmail.com>
2016-12-11 16:34:31 -0500

    Revert "Started Action For Refresh Button"
    This reverts commit 93dee45d57a69d8c535086b4b962db5208826f5e.

--- src/Client/ClientWindow.java
+++ src/Client/ClientWindow.java
@@ -30,8 +30,6 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JLabel;
 
-import Server.ClientHandler;
-
 public class ClientWindow {
 
 	private JFrame frmClient;
@@ -174,16 +172,7 @@ public class ClientWindow {
 					osw.flush();
 					while(true){
 					if(!scan.nextLine().equals("Refreshed")){
-						//call getLatest
-						//currently accesses the local files array to loop
-						//but if the server got lots of updates before this client refreshes...
-						//then this loop will stop at the size of its previously saved array
-						for(int i=0;i<files.size();i++)
-						{
-							files.get(i).lastUser = scan.nextLine();
-							files.get(i).dateEdited = scan.nextLine();
-							
-						}
+						
 					}
 					}
 				} catch (IOException e1) {
