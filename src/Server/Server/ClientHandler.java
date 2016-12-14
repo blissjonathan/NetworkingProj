fe353c7488d0d72315eaec98a0abf53cf5b6c893
Jonathan Bliss <blissjonathan1@gmail.com>
2016-12-14 12:59:33 -0500

    Refix

--- src/Server/Server/ClientHandler.java
+++ src/Server/Server/ClientHandler.java
@@ -103,7 +103,6 @@ public class ClientHandler extends Thread
     }
     
     public void getLatest() throws IOException {
-    	Server.getAllFiles();
     	ArrayList<clientFile> getList = Server.files;
     	for(int i = 0; i  < getList.size(); i++) {
 				osw.write(getList.get(i).getName() + "\r\n");
